395db5f5c6593f92a724ef403ea94515a77c540f
VinniePazz <dimasmakouz@gmail.com>
2020-06-24 19:49:44 +0300

    Add mongo setup

---
+++ database/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/main?retryWrites=true&w=majority`;
+
+module.exports = () => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+};
--- server.js
+++ server.js
@@ -2,29 +2,39 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 require('dotenv').config();
+const connectMongo = require('./database');
+
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
-// import routes
-const authRoutes = require('./routes/auth');
-
-// app middlewares
+// middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-// app.use(cors()); // allows all origins
 if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+  // app.use(cors()); // allows all origins
+  app.use(cors({ origin: `${process.env.PUBLIC_URL}` }));
 }
 
-// middleware
+// routes
 app.use('/api', authRoutes);
 
-// port
-const port = process.env.PORT || 8000;
+// mount server
+const mountServer = async () => {
+  const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`app is running on port ${port}`);
-});
+  try {
+    await connectMongo();
+    console.log('connected to MongoDB Cluster');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+  });
+};
+
+mountServer();
