17af83364ca94234937e217392f5c1d40c1b06d8
VinniePazz <dimasmakouz@gmail.com>
2020-06-24 19:40:42 +0300

    Revert "Add mongodb setup"
    This reverts commit 60d518d014279facc039f7fcd09d770387fe21cf.

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectMongo = require('./database');
+const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth');
-
-const app = express();
 
 // app middlewares
 app.use(morgan('dev'));
@@ -17,26 +16,15 @@ app.use(bodyParser.json());
 
 // app.use(cors()); // allows all origins
 if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: `${process.env.PUBLIC_URL}` }));
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
 // middleware
 app.use('/api', authRoutes);
 
-// mount server
-const startServer = async () => {
-  const port = process.env.PORT || 8000;
+// port
+const port = process.env.PORT || 8000;
 
-  try {
-    await connectMongo();
-    console.log('connected to MongoDB Cluster');
-  } catch (err) {
-    console.error(err);
-  }
-
-  app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
-  });
-};
-
-startServer();
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
