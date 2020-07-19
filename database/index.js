8200c5eaeeeafc23e1f59e576810a8d9c1a8587f
VinniePazz <dimasmakouz@gmail.com>
2020-07-19 15:55:29 +0300

    Change mongoose connection

--- database/index.js
+++ database/index.js
@@ -2,12 +2,21 @@ const mongoose = require('mongoose');
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/main?retryWrites=true&w=majority`;
 
+const mongooseOptions = {
+  promiseLibrary: global.Promise,
+  poolSize: 50,
+  keepAlive: 30000,
+  connectTimeoutMS: 5000,
+  reconnectTries: Number.MAX_SAFE_INTEGER,
+  reconnectInterval: 5000,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
 function connectToMongoDB() {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  return mongoose.connect(url, mongooseOptions);
 }
 
 module.exports = connectToMongoDB;
