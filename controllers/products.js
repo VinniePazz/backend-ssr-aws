0e184cde42619bd8c164ed8d5ac83b3af33baceb
VinniePazz <dimasmakouz@gmail.com>
2020-07-27 11:35:46 +0300

    Add new models

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,13 @@ const Product = require('../models/Product');
 
 exports.getAllProducts = (req, res, next) => {
   if (req.query) {
+    console.log(req);
     next();
     return;
   }
   Product.find({})
     .then((products) => {
+      console.log(products);
       res.json({ status: 'success', data: products });
     })
     .catch((err) => {
@@ -15,6 +17,9 @@ exports.getAllProducts = (req, res, next) => {
 };
 
 exports.getProductsByFilter = (req, res, next) => {
+  if (!req.query) {
+    return;
+  }
   const search = /сма/gi;
   Product.find({ $or: [{ name: search }, { description: search }] })
     .then((products) => {
@@ -23,4 +28,10 @@ exports.getProductsByFilter = (req, res, next) => {
     .catch((err) => {
       res.status(404).json({ success: false, error: err });
     });
+};
+
+exports.getProduct = async (req, res, next) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+  res.json({ status: 'success', data: product });
 };
--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 // import controller
 const {
+  getProduct,
   getAllProducts,
   getProductsByFilter,
 } = require('../controllers/products');
 
 router.get('/', getAllProducts);
 router.get('/', getProductsByFilter);
+router.get('/:id', getProduct);
 
 module.exports = router;
